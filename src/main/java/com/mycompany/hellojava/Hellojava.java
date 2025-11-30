39555dca621a6e7f5712acdc01da52c9d03a672b
Webert <webert.feitosaesouza@outlook.com>
2025-11-30 00:54:32 -0500

    comite teste no netbeans

--- src/main/java/com/mycompany/hellojava/Hellojava.java
+++ src/main/java/com/mycompany/hellojava/Hellojava.java
@@ -13,6 +13,7 @@ public class Hellojava {
     public static void main(String[] args) {
         System.out.println("Meu Java está rodando no linux mint, luz câmera e ação programador!");
         System.out.println("çãÂéêẽioóutñ");
-        System.out.println("teste de git e github");
+        System.out.println("teste de git e github no terminal");
+        System.out.println("teste no netbeans");
     }
 }
