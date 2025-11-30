03a0f5f5f6cc9438f3c97c085ac041d6f5907b92
Webert <webert.feitosaesouza@outlook.com>
2025-11-29 22:50:47 -0500

    Primeiro commit - projeto java

---
+++ src/main/java/com/mycompany/hellojava/Hellojava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hellojava;
+
+/**
+ *
+ * @author webert
+ */
+public class Hellojava {
+
+    public static void main(String[] args) {
+        System.out.println("Meu Java está rodando no linux mint, luz câmera e ação programador!");
+        System.out.println("çãÂéêẽioóutñ");
+    }
+}
